32568a198be4e3b689ff32f732612a2a7c4c634f
Talha Shehzore Khan <shehzoret89@gmail.com>
2024-10-02 06:30:57 +0500

    Some Work Remaining

---
+++ ASP.NET_Core_MVC_Practical/Controllers/GenresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_MVC_Practical.Controllers
+{
+    public class GenresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
